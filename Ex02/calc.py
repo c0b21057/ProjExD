f3459ddf9d6bad68854b8f05f2171b02c1b7c938
rikuho kiriyama <c0b21057bc@edu.teu.ac.jp>
2022-10-04 16:49:14 +0900

    機能追加済み

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -1,31 +1,37 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def click_number(event): # 練習3
+def click_number(event): 
     btn = event.widget
     num = btn["text"]
-    #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
-    entry.insert(tk.END, num) # 練習5
+    entry.insert(tk.END, num) 
 
 
 def click_equal(event):
-    eqn = entry.get()
-    res = eval(eqn)
+    try:
+        eqn = entry.get()
+        res = eval(eqn)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
+    except ZeroDivisionError:
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, "計算不可")
+
+def click_allclear(event):
     entry.delete(0, tk.END)
-    entry.insert(tk.END, res) # 練習7
 
 
-root = tk.Tk() # 練習1
+root = tk.Tk() 
 root.geometry("300x500")
 
-entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") 
 entry.grid(row=0, column=0, columnspan=3)
 
-r, c = 1, 0 # r: 行を表す変数／c：列を表す変数
-numbers = list(range(9, -1, -1)) # 数字だけのリスト
-operators = ["+"] # 演算子だけのリスト
+r, c = 1, 0 
+numbers = list(range(9, -1, -1)) 
+operators = ["+","-","*","/","(",")"] 
 for i, num in enumerate(numbers+operators, 1):
-    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=1)
     btn.bind("<1>", click_number)
     btn.grid(row=r, column=c)
     c += 1
@@ -33,8 +39,12 @@ for i, num in enumerate(numbers+operators, 1):
         r += 1
         c = 0
 
-btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=2)
+btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=1)
 btn.bind("<1>", click_equal)
 btn.grid(row=r, column=c)
+
+btn = tk.Button(root, text=f"AC", font=("", 30), width=4, height=1)
+btn.bind("<1>", click_allclear)
+btn.grid(row=6, column=2)
 
 root.mainloop()
