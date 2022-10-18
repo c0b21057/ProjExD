06e12956c355c323198c44adfbc63d9473b176f2
rikuho kiriyama <c0b21057bc@edu.teu.ac.jp>
2022-10-18 14:31:48 +0900

    練習1-6

--- maze.py
+++ maze.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+def key_down(event):
+    key=event.keysym
+
+
+def key_up(event):
+    key=event.keysym
+
+
 if __name__ =="__main__":
     root=tk.Tk()
     root.title("迷えるこうかとん")
@@ -11,5 +19,11 @@ if __name__ =="__main__":
     tori=tk.PhotoImage(file="fig/5.png")
     cx,cy=390,400
     canv.create_image(cx,cy,image=tori,tag="")
+
+    #キーが押されたときの設定
+    key=""
+    root.bind("<KeyPress>",key_down)
+
+    root.bind("<KeyRelease>",key_up)
 
     root.mainloop()
